2b4034a9dcdc50866d593d35622860867b01c404
LAI <lukalai1@orange.fr>
2022-10-12 10:30:50 +0200

    commit ajout hook sur deuxieme champ

--- src/components/SimpleInput2.js
+++ src/components/SimpleInput2.js
@@ -1,8 +1,10 @@
-import { useState } from 'react'; 
+//import { useState } from 'react'; 
 import useInput from '../hooks/UseInput.js'; 
 
 const SimpleInput2 = (props) => { 
 
+
+  // Utilisation du hook pour input name
   const { 
     value: enteredName, 
     isValid: enteredNameIsValid, 
@@ -13,24 +15,35 @@ const SimpleInput2 = (props) => {
 
   } = useInput(value => value.trim() !== ''); 
 
+  const { 
+    value: enteredMail, 
+    isValid: enteredMailIsValid, 
+    hasError: mailInputHasError, 
+    valueChangeHandler: mailChangedHandler, 
+    inputBlurHandler: mailBlurHandler, 
+    reset: resetMailInput 
+
+  } = useInput(value => value.trim() !== ''); 
+
+/*
   const [enteredEmail, setEnteredEmail] = useState(''); // Hook useState
   const [enteredEmailTouched, setEnteredEmailTouched] = useState(false); 
   const enteredEmailIsValid = enteredEmail.includes('@'); 
   const enteredEmailIsInvalid = !enteredEmailIsValid && enteredEmailTouched; 
-  let formIsValid = false;  
+ */ let formIsValid = false;  
 
-  if (enteredNameIsValid && enteredEmailIsValid) { // Valide le formulaire si name <> '' et si mail contient @
+  if (enteredNameIsValid && enteredMailIsValid) { // Valide le formulaire si name <> '' et si mail contient @
     formIsValid = true; 
   } 
 
-  const emailInputChangeHandler = (event) => { // Fonction recupere la valeur du champ a chaque modif
+ /* const emailInputChangeHandler = (event) => { // Fonction recupere la valeur du champ a chaque modif
     setEnteredEmail(event.target.value); 
   }; 
 
   const emailInputBlurHandler = (event) => { // Fonction sortie du champ email si 
     setEnteredEmailTouched(true); //Envoie bool vrai champ email touché
   }; 
-
+*/
   const formSubmissionHandler = (event) => { //fonction submit form
     event.preventDefault(); // recupere event
     if (!enteredNameIsValid) { 
@@ -39,15 +52,16 @@ const SimpleInput2 = (props) => {
     console.log(enteredName); 
     // nameInputRef.current.value = ''; => NOT IDEAL, DON'T MANIPULATE THE DOM 
     resetNameInput(); // met à vide le champ name
-    setEnteredEmail(''); // hook usestate qui met caractere vide champ email
-    setEnteredEmailTouched(false); // reinitialise bool
+    resetMailInput(); // met a vide le champ mail
+   // setEnteredEmail(''); // hook usestate qui met caractere vide champ email
+  // setEnteredEmailTouched(false); // reinitialise bool
   }; 
 
   const nameInputClasses = nameInputHasError 
     ? 'form-control invalid' 
     : 'form-control'; 
 
-  const emailInputClasses = enteredEmailIsInvalid 
+  const emailInputClasses = mailInputHasError
     ? 'form-control invalid' 
     : 'form-control'; 
 
@@ -72,11 +86,11 @@ const SimpleInput2 = (props) => {
         <input 
           type='email' 
           id='email' 
-          onChange={emailInputChangeHandler} 
-          onBlur={emailInputBlurHandler} 
-          value={enteredEmail} 
+          onChange={mailChangedHandler} 
+          onBlur={mailBlurHandler} 
+          value={enteredMail} 
         /> 
-        {enteredEmailIsInvalid && ( 
+        {enteredMailIsValid && ( 
           <p className='error-text'>Please enter a valid email.</p> 
         )} 
       </div> 
